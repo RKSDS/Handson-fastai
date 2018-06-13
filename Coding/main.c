f8a1c3e11af6286bd3a3eca8fc22f58debffba10
Ramesh Singh <ramesh.singh1986@gmail.com>
2018-06-13 09:02:55 +0530

    Code for string function implementation strchr and strlen

--- Coding/main.c
+++ Coding/main.c
@@ -6,5 +6,33 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	pointerArithmetic();
+	/* pointerArithmetic(); */
+	char *newstring = "This is an example";
+	int str_length;
+	str_length = ustrlen(newstring);
+	printf("%d string length\n", str_length);
+	
+	newstring = "How are you?";
+	str_length = ustrlen(newstring);
+	printf("%d string length\n", str_length);
+	
+	newstring = NULL;
+	str_length = ustrlen(newstring);
+	printf("%d string length\n", str_length);
+	
+	newstring = "";
+	str_length = ustrlen(newstring);
+	printf("%d string length\n", str_length);
+	
+	newstring = "aeiou";
+	char * ch;
+	ch = mystrchr(newstring, 'i');
+	printf("%c character found\n", ch);
+	
+	ch = mystrchr("HELLO", 'O');
+	
+	printf("strchr: %c my_strchr: %c \n", *strchr("HELLO", 'O'), ch);
+	
+	ch = mystrchr("HELLO", 'H');
+  	printf("strchr: %c my_strchr: %c \n", *strchr("HELLO", 'H'), ch);	
 }
--- Coding/pointer.c
+++ Coding/pointer.c
@@ -1,5 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int arrayEx() {
+	char chararr[4] = {'a', 'b', 'c', 'd'};
+	printf("%p = address of chararr\n", &chararr);
+	printf("%p = address stored in chararr\n", chararr);
+	printf("%p = address of 1st character\n", &chararr[0]);
+	printf("%p = address of 2nd character\n", &chararr[1]);
+	printf("%p = address of 3nd character\n", &chararr[2]);
+	printf("%p = address of 4nd character\n", &chararr[3]);
+	system("PAUSE");
+	return 0;
+}
 
 int pointerEx() {
 	int radius = 45;
@@ -18,4 +32,53 @@ int pointerEx() {
 	and the pointer address is something different*/
 	system("PAUSE");
 	return 0;
+}
+
+int pointerArithmetic() {
+	int arr[4] = {10, 20, 30, 40};
+	int *arrptr = arr;
+	printf("%d = value of 1st element, %p = address of 1st element\n", *arrptr, arrptr);
+	arrptr++;
+	printf("%d = value of 2nd element, %p = address of 2nd element\n", *(arrptr), arrptr);
+	int y = *(arrptr + 1);
+	printf("%d = value of 3rd element, %p = address of 2rd element\n", y, arrptr);
+	*arrptr++;
+	printf("%d = value of 3rd element, %p = address of 3rd element\n", *(arrptr), arrptr);
+	int x = (*arrptr)+1;
+	printf("%d = value of new element, %p = address of new element\n", x, arrptr);
+	
+	system("PAUSE");
+	return 0;
+}
+
+size_t ustrlen(const char *str){
+	if(str == NULL){
+		return 0;
+	}
+	
+	size_t length=0;
+	while(*str!='\0')
+	{
+		str+=1;
+		length +=1;
+	}
+	return length;
+}
+
+char * mystrchr(char * str, int c)
+{
+	if(str == NULL)
+	{
+		return NULL;
+	}
+	
+	while(*str != '\0')
+	{
+		if(*str == c)
+		{
+			return (char *) str;
+		}
+		str++;
+	}
+	return NULL;
 }
